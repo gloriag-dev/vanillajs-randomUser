2a5ec86940998520a5cbbc7b836b156142c1632f
Gloria Giannascoli <giannascoli.gloria@gmail.com>
2022-09-11 10:18:16 +0200

    fix

--- utils/fetchUser.js
+++ utils/fetchUser.js
@@ -3,7 +3,6 @@ const URL = 'https://randomuser.me/api/';
 const getUser = async () => {
   const response = await fetch(URL);
   const data = await response.json();
-  // destructure
   const person = data.results[0];
   const { phone, email } = person;
   const { large: image } = person.picture;
